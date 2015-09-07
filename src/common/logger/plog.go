b20cc9497104415c4ce31c68ff87e43ec4c31f83
liuke <xyliuke@gmail.com>
2015-09-07 23:35:40 +0800

    添加log的其他级别

--- src/common/logger/plog.go
+++ src/common/logger/plog.go
@@ -1,9 +1,10 @@
 package logger
 
 import (
+	"bytes"
 	"fmt"
-	_ "io"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -39,12 +40,13 @@ func (log Logger) LogI(msg string) {
 	log.writeLog2File(msg, "info")
 }
 
-func LogW() {
-
+func (log Logger) LogW(msg string) {
+	log.writeLog2File(msg, "warn")
 }
 
-func LogE() {
-
+func (log Logger) LogE(msg string) {
+	fmt.Println("loge")
+	log.writeLog2File(msg, "error")
 }
 
 func PLog(logType string) *Logger {
@@ -52,16 +54,26 @@ func PLog(logType string) *Logger {
 	if ok {
 		return logger
 	}
+	fmt.Println("Plog", logger)
 	return nil
 }
 
 func (log *Logger) writeLog2File(msg string, level string) {
-	log.file.WriteString("[" + time.Now().String() + "]" + level + ":\t" + msg + "\n")
+	t := time.Now()
+	var buf bytes.Buffer
+	buf.WriteString("[")
+	buf.WriteString(t.Format("2006-01-02 15:04:05."))
+	buf.WriteString(strconv.Itoa(t.Nanosecond()))
+	buf.WriteString("]")
+	buf.WriteString(level)
+	buf.WriteString(":\t")
+	buf.WriteString(msg)
+	buf.WriteString("\n")
+	buf.WriteTo(log.file)
 }
 
 func getWriteByLog(logType string) (*Logger, bool) {
 	logger, ok := loggerMap[logType]
-	fmt.Println("log file map :", logger.file, ok)
 	if ok {
 		if logger.file == nil || logger.isShouldCreateNewLogFile() {
 			file, err := os.OpenFile(getLogFileAbsoultPath(logger.fileName), os.O_RDWR|os.O_APPEND|os.O_CREATE, os.ModePerm)
--- src/common/logger/plog_test.go
+++ src/common/logger/plog_test.go
@@ -5,7 +5,9 @@ import (
 )
 
 func Test_WriteLog(t *testing.T) {
-	AddLog("info", "info.log")
+	AddLog("info", "app_info.log")
 	PLog("info").LogI("msg test test")
 	PLog("info").LogI("msg test test 12323213")
+	PLog("info").LogW("msg test test 12323213")
+	PLog("info").LogE("msg test test 12323213")
 }
