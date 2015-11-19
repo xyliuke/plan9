1d5bbf1c374e2c049f9d52fb68a4bf58f1493685
liuke <xyliuke@gmail.com>
2015-11-19 10:09:32 +0800

    完成json检测功能
    json检测功能是对lua中的table数据进行测试，用以判断所给的数据是否符合协议，如果不符合协议，则打印出错误。
    在data_check_style文件中定义了例子样式，可参考这个来写自己所需要的协议格式

--- common/lua_bind/data_check_style.lua
+++ common/lua_bind/data_check_style.lua
@@ -10,19 +10,35 @@ local dt = {"number", "string", "boolean", "object", "array"}
 
 --  样式控制的属性都是以__property_开头
 example_style = {
-	a = {
+	key_object = {
 			__property_type__ = dt[4], --数据类型
 			__property_require__ = true,  -- 是否必要
 			__property_comment__ = "注释",
-			-- 如果a为object类型，则需要包含其他的数据
-			b = 
-			{
-				__property_type__ = dt[1],
-				__property_require__ = false,
+			-- 如果为object类型，则需要包含其他的数据
+			key_string = {
+				__property_type__ = dt[2],
+				__property_require__ = true,
+			},
+			key_object = {
+				__property_type__ = dt[4],
+				__property_require__ = true,
+				key_arr1 = {
+					__property_type__ = dt[5],
+					__property_require__ = true,
+				}
 			}
 		},
-	c = {
-			__property_type__ = dt[1], --数据类型
-			__property_require__ = true,  -- 是否必要
-		}
+	key_arr = {
+			__property_type__ = dt[5],
+			__property_require__ = true,
+		},
+	key_bool = {
+			__property_type__ = dt[3],
+			__property_require__ = true
+	},
+	key_number = {
+			__property_require__ = false,
+			__property_type__ = dt[1]
+	}
+
 }
--- common/lua_bind/table_check.lua
+++ common/lua_bind/table_check.lua
@@ -4,31 +4,25 @@
 
 require "data_check_style"
 
---- 检测所给的json数据是否与指定的样式符合
--- @param data 数据
--- @param style 样式结构，见data_check_style提示
--- @return 样式符合返回true；否则返回false
-function json_check(data, style)
-	if data == nil or style == nil then
-		return false
-	end
-	for k,v in pairs(style) do
-		print(k, ",type:", type(k), "\tvalue", v, ",type:", type(v))
-		local sk = style[k]
-		if sk then
 
-		end
+local function is_style_property(key)
+	local index, len = string.find(key, "__property_")
+	if index == 1 then
+		return true
+	else
 		return false
 	end
-	return false
 end
 
 --- 判断val的类型，返回类型
 -- @prarm val 数据
 -- @return 返回值为string/boolean/number/object/array中其一
-function json_type(val)
+local function json_type(val)
 	t = type(val)
 	if t == "table" then
+		if #val == 0 then
+			return "object"
+		end
 		for k,v in pairs(val) do
 			if type(k) ~= "number" then
 				return "object"
@@ -40,27 +34,108 @@ function json_type(val)
 	end
 end
 
-function json_match(val, style)
-	if json_type(val) == style.__property_type__ then
 
+--- 检测json数据是否符合style标准，如果json数据是style的超集（即style中所有数据都在json中），也判断为符合
+-- @param data 数据
+-- @param style 样式结构，见data_check_style提示
+-- @return 样式符合返回true,nil；否则返回false,"reason"
+local function json_check_json_from_style(data, style)
+	for k,v in pairs(style) do
+		if not is_style_property(k) then
+			if v["__property_require__"] == nil then
+				return false, k .. " value must has __property_require__ in style"
+			end
+			if v["__property_type__"] == nil then
+				return false, k .. " must has __property_type__ in style"
+			end
+			if v["__property_require__"] then
+				local d_value = data[k]
+				if d_value == nil then
+					return false, k .. " is not exist in json"
+				end
+				local j_type = json_type(d_value)
+				local s_type = v["__property_type__"]
+				if j_type ~= s_type then
+					return false, k .. " value type " .. j_type .. " is not match style type " .. s_type
+				end
+				if s_type == "object" then
+					local result, reason = json_check_json_from_style(d_value, v)
+					if not result then
+						return result, reason
+					end
+				end
+			else
+				local d_value = data[k]
+				if d_value then
+					local j_type = json_type(d_value)
+					local s_type = v["__property_type__"]
+					if j_type ~= s_type then
+						return false, k .. " value type " .. j_type .. " is not match style type " .. s_type
+					end
+				end
+			end
+		end
 	end
+	return true, "success"
 end
 
-local d = {a = "va", b = "vb", c = 1, d = 1.2, e = {e1 = "ve"}, f = {1,2,3,4}, g = true}
-print(d["a"])
--- json_check(d)
+--- 和json_check_json_from_style相反，检测json中是否有数据不在style标准中的，如果存在则判断为不符合
+-- @param data 数据
+-- @param style 样式结构，见data_check_style提示
+-- @return 样式符合返回true；否则返回false
+local function json_check_style_from_json(data, style)
+	for k,v in pairs(data) do
+		local d_type = json_type(v)
+		local s_value = style[k]
+		if s_value == nil then
+			return false, k .. " is not define in style"
+		end
+		if d_type == "object" then
+			local result, reason = json_check_style_from_json(v, s_value)
+			if not result then
+				return result, reason
+			end
+		end
+	end
+	return true, "success"
+end
 
--- local arr = {1,2,3,4,5, a = "a"}
--- local x = 1
--- local y = 1.2
--- local z = "hello"
--- local g = true
--- -- print(json_check(arr))
--- print(json_type(arr))
--- print(json_type(x))
--- print(json_type(y))
--- print(json_type(z))
--- print(json_type(g))
+
+
+--- 检测所给的json数据是否与指定的样式符合
+-- @param data 数据
+-- @param style 样式结构，见data_check_style提示
+-- @return 样式符合返回true,nil；否则返回false,"reason"
+function json_check(data, style)
+	if data == nil or style == nil then
+		return false
+	end
+	local ret, reason = json_check_json_from_style(data, style)
+	if ret then
+		ret, reason = json_check_style_from_json(data, style)
+	end
+	return ret, reason
+end
+
+-- 测试数据
+local d = {
+	key_object = {
+		key_string = "string", 
+		key_object = {
+			key_arr1 = {"1", "a", "b"},
+			-- key_test = ""
+		}, 
+	},
+	key_arr = {1,3,4},
+	key_bool = true,
+	key_number = 1.2,
+	-- key_a = "d"
+}
+
+-- 测试调用
+local result, reason = json_check(d, example_style)
+print(result, reason)
+
 
 
 
