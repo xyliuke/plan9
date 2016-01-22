16ee27b3bb07346be52b0928a103c564d2c782f5
liuke <xyliuke@gmail.com>
2016-01-22 09:33:40 +0800

    服务器添加生成uuid库

--- server/src/common/util/uuid_test.go
+++ server/src/common/util/uuid_test.go
@@ -1,22 +1,25 @@
 package util
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
-
-
 func Test_CreateID(t *testing.T) {
-	var id string;
+	var id string
 	id = CreateID()
-	fmt.Println(id);
+	fmt.Println(id)
 }
 
 func Benchmark_CreateID(b *testing.B) {
-	var id string;
-	for i := 0; i < 50000; i ++ {
+	var id string
+	for i := 0; i < 50000; i++ {
 		id = CreateID()
 	}
-	fmt.Println(id);
+	fmt.Println(id)
+}
+
+func Test_UUID(t *testing.T) {
+	id := NewUUID()
+	fmt.Println("uuid : ", id)
 }
--- server/src/server/tcp/tcp_server_test.go
+++ server/src/server/tcp/tcp_server_test.go
@@ -3,40 +3,10 @@ package tcp
 
 import (
 	"testing"
-	_ "fmt"
+//	"fmt"
 )
 
-//var i int
-
 func Test_CreateTcpServer(t *testing.T)  {
-//	data := make([]byte, 20)
-//	data[0] = 50
-//	data[1] = '3'
-//
-//	data[2] = 60
-//	data[3] = 61
-//	data[1] = '\r'
-//	data[2] = '\n'
-
-//	data1 := make([]byte, 200)
-//	data1[0] = 'a'
-//	data1[1] = 'b'
-
-//	data1 = append(data1, data...)
-
-//	l := 2
-//	tmp := data[2:3]
-//	fmt.Println(tmp)
-//
-//	fmt.Println(data)
-//	copy(data[0:], data[2:])
-//	fmt.Println(data)
-
-
-
-//	fmt.Println(i)
-//	i ++
-//	fmt.Println(i)
-	tp := CreateTcpServer(8081)
-	tp.Listen()
+//	tp := CreateTcpServer(8081)
+//	tp.Listen()
 }
