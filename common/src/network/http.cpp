ecd2318e5935e2f956dd75e900702e349068a056
liuke <xyliuke@gmail.com>
2016-09-22 15:16:21 +0800

    Merge branch 'master' of https://github.com/xyliuke/plan9
    # Conflicts:
    #	common/src/network/asyn_http.cpp
    #	common/src/network/asyn_http.h

--- common/src/network/http.cpp
+++ common/src/network/http.cpp
@@ -1,202 +1,300 @@
 //
-// Created by liuke on 16/6/3.
+// Created by guazi on 8/4/16.
 //
 
 #include "http.h"
-#include <ostream>
+#include <curl/curl.h>
+#include <cstdlib>
 #include <iostream>
-#include <boost/filesystem.hpp>
-
-#ifdef __i386__
-#include <include-i386/curl/curl.h>
-#elif __x86_64__
-#include <include-x86_64/curl/curl.h>
 #include <fstream>
+#include <sstream>
 
-#elif __arm__
-#include <include-armv7/curl/curl.h>
-#elif __arm64__
-#include <curl/include-arm64/curl/curl.h>
-#endif
-
-namespace plan9
-{
-
-    class write_buf {
+namespace plan9 {
+    class http_object {
     public:
-        void* buf;
-        size_t cap;
-        size_t len;
-    };
-
-    class progress {
-    public:
-        double lastruntime;
         CURL *curl;
-        std::function<void(long, long)> callback;
+        char *data;
+        size_t len;
+        size_t cap;
+        double time;
+        char header[1024];
+        size_t header_len;
+
+        std::function<void(long, long)> download_progress_callback;
         curl_off_t downloaded;
         curl_off_t download_total;
-        curl_off_t uploaded;
-        curl_off_t upload_total;
-        bool is_download;//是download模式为true,否则为upload模式
+        std::shared_ptr<std::ofstream> ofstream;
+
+        std::stringstream debug_stream;
     };
 
-    static size_t write_data_callback(void *buffer, size_t size, size_t nmemb, void *userp) {
-        write_buf * writeBuf = (write_buf *)userp;
-        size_t s = size * nmemb;
-        size_t cap = writeBuf->cap;
-        while (s > (cap - writeBuf->len)) {
-            cap *= 2;
-        }
-        writeBuf->cap = cap;
-        writeBuf->buf = realloc(writeBuf->buf, cap);
-        memcpy((char*)writeBuf->buf + writeBuf->len, buffer, s);
-        writeBuf->len += s;
-        return s;
-    }
+    static size_t write_callback(char *data, size_t n, size_t l, void *p) {
+        http_object *h = (http_object *) p;
+        size_t clen = n * l;
+        if (h->cap == 0) {
+            h->cap = clen * 2;
+            h->data = (char *) malloc(h->cap);
+        } else if ((h->cap - h->len) < clen) {
+            size_t cap_tmp = h->cap * 2;
 
-    static size_t write_data_to_file_callback(void *buffer, size_t size, size_t nmemb, void *userp) {
-        size_t s = size * nmemb;
-        std::ofstream* ofstream = (std::ofstream*)userp;
-        ofstream->write((char*)buffer, s);
-        return s;
-    }
-
-    static int progress_callback(void *clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow) {
-        progress *myp = (progress*)clientp;
-        CURL *curl = myp->curl;
-        double curtime = 0;
-
-        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &curtime);
-        myp->lastruntime = curtime;
-        if (myp->callback != nullptr) {
-            if (myp->is_download && myp->downloaded != dlnow) {
-                myp->downloaded = dlnow;
-                myp->download_total = dltotal;
-                myp->callback(dlnow, dltotal);
-            } else if (!myp->is_download && myp->uploaded != ulnow) {
-                myp->uploaded = ulnow;
-                myp->upload_total = ultotal;
-                myp->callback(ulnow, ultotal);
+            while ((cap_tmp - h->len) < clen) {
+                cap_tmp *= 2;
             }
+            h->cap = cap_tmp;
+            h->data = (char *) realloc(h->data, cap_tmp);
         }
 
-        if((dlnow != 0 && dlnow == dltotal) || (ulnow != 0 && ulnow == ultotal)) {
-            //不再执行
-            return 1;
+        std::copy(data, data + clen, h->data + h->len);
+        h->len += clen;
+
+        return clen;
+    }
+
+    static size_t header_callback(char *data, size_t n, size_t l, void *p) {
+        http_object *h = (http_object *) p;
+        size_t clen = n * l;
+        std::copy(data, data + clen, h->header + h->header_len);
+        h->header_len += clen;
+        return clen;
+    }
+
+    static size_t read_callback(void *ptr, size_t size, size_t nmemb, void *userp)
+    {
+        if(size*nmemb < 1)
+            return 0;
+        return 0;
+    }
+
+    static int debug_callback(CURL *handle, curl_infotype type, char *data, size_t size, void *userp) {
+        auto ho = (http_object*)userp;
+        switch (type) {
+            case CURLINFO_TEXT:
+                ho->debug_stream << std::string(data, size);
+                break;
+            case CURLINFO_HEADER_OUT:
+                ho->debug_stream << std::string(data, size);
+                break;
+            case CURLINFO_DATA_OUT:
+                break;
+            case CURLINFO_SSL_DATA_OUT:
+                break;
+            case CURLINFO_HEADER_IN:
+                ho->debug_stream << std::string(data, size);
+                break;
+            case CURLINFO_DATA_IN:
+                break;
+            case CURLINFO_SSL_DATA_IN:
+                break;
+            case CURLINFO_END:
+                break;
+            default:
+                break;
         }
         return 0;
     }
 
+    static size_t download_callback(char *data, size_t n, size_t l, void *p) {
+        http_object* h = (http_object *) p;
+        size_t clen = n * l;
 
-    class http::http_impl
-    {
+        h->ofstream->write(data, clen);
+
+        return clen;
+    }
+
+    static size_t progress_callback(void *p, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow) {
+        http_object *ho = (http_object *) p;
+
+        double curtime = 0;
+        curl_easy_getinfo(ho->curl, CURLINFO_TOTAL_TIME, &curtime);
+        ho->time = curtime;
+
+        if (ho->downloaded != dlnow && dltotal != 0) {
+            ho->downloaded = dlnow;
+            ho->download_total = dltotal;
+            ho->download_progress_callback(dlnow, dltotal);
+        }
+
+        return (dltotal != 0 && dltotal == dlnow) ? 1 : 0;
+    }
+
+
+    class http::http_impl {
     public:
-
         http_impl() {
             curl_global_init(CURL_GLOBAL_ALL);
+//        curlm = curl_multi_init();
+//        curl_multi_setopt(curlm, CURLMOPT_MAXCONNECTS, 2L);
         }
 
         ~http_impl() {
+//        curl_multi_cleanup(curlm);
             curl_global_cleanup();
         }
 
-        void get(std::string url, std::function<void(char *, size_t)> callback) {
+        void get(std::string url, std::map<std::string, std::string>* header, std::function<void(std::string header, char *, size_t)> callback) {
+            http_object ho;
+            CURL *curl = curl_easy_init();
+            ho.curl = curl;
+            ho.cap = 0;
+            ho.len = 0;
+            ho.header_len = 0;
 
-            write_buf writeBuf;
-            writeBuf.cap = 1024;
-            writeBuf.buf = malloc(writeBuf.cap);
-            writeBuf.len = 0;
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ho);
+            curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 
-            CURL* curl_ = curl_easy_init();
-            curl_easy_setopt(curl_, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, write_data_callback);
-            curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &writeBuf);
-            curl_easy_setopt(curl_, CURLOPT_FOLLOWLOCATION, 1);
-            curl_easy_setopt(curl_, CURLOPT_FORBID_REUSE, 1);
+            curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
+            curl_easy_setopt(curl, CURLOPT_HEADERDATA, &ho);
 
-            CURLcode ret = curl_easy_perform(curl_);
-            if (ret == CURLE_OK) {
-                callback((char*)(writeBuf.buf), writeBuf.len);
+            curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, &debug_callback);
+            curl_easy_setopt(curl, CURLOPT_DEBUGDATA, &ho);
+
+            struct curl_slist* list = NULL;
+            if (header != nullptr) {
+                std::map<std::string, std::string>::const_iterator iterator = header->begin();
+                while (iterator != header->end()) {
+                    list = curl_slist_append(list, (iterator->first + ":" + iterator->second).c_str());
+                    iterator ++;
+                }
+
+                curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
             }
-            curl_easy_cleanup(curl_);
+
+            if (curl_easy_perform(curl) == CURLE_OK) {
+                if (callback != nullptr) {
+                    callback(std::string(ho.header, ho.header_len), ho.data, ho.len);
+                }
+            } else {
+                if (callback != nullptr) {
+                    callback(std::string(ho.header, ho.header_len), (char *) 0, -1);
+                }
+            }
+
+            curl_easy_cleanup(curl);
+            if (list != NULL) {
+                curl_slist_free_all(list);
+            }
+
+            std::cout << ho.debug_stream.str() << std::endl;
         }
 
-        bool get(std::string url, std::string file, std::function<void(long, long)> process) {
+        void download(std::string url, std::string path, std::function<void(long downloaded, long total)> callback) {
+            http_object ho;
+            ho.len = 0;
+            ho.cap = 0;
+            ho.download_progress_callback = callback;
+            ho.downloaded = 0;
+            ho.download_total = 0;
+            ho.ofstream.reset(new std::ofstream);
+            ho.ofstream->open(path, std::ios::trunc);
 
-            namespace bs = boost::filesystem;
-            bs::path p(file);
-            if (!bs::exists(p.parent_path())) {
-                bs::create_directories(p.parent_path());
-            } else if (bs::is_regular_file(p.parent_path())){
-                return false;
-            }
+            CURL *curl = curl_easy_init();
+            ho.curl = curl;
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, download_callback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ho);
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+            curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, progress_callback);
+            curl_easy_setopt(curl, CURLOPT_XFERINFODATA, &ho);
+            curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
 
-            std::ofstream ofstream;
-            ofstream.open(file, std::ios::trunc);
+            curl_easy_perform(curl);
 
-            CURL* curl_ = curl_easy_init();
-            curl_easy_setopt(curl_, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, write_data_to_file_callback);
-            curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &ofstream);
-            curl_easy_setopt(curl_, CURLOPT_FOLLOWLOCATION, 1);
-            curl_easy_setopt(curl_, CURLOPT_FORBID_REUSE, 1);
-
-            curl_easy_setopt(curl_, CURLOPT_XFERINFOFUNCTION, progress_callback);
-            progress prog;
-            prog.is_download = true;
-            prog.curl = curl_;
-            prog.lastruntime = 0;
-            prog.callback = process;
-            curl_easy_setopt(curl_, CURLOPT_XFERINFODATA, &prog);
-            curl_easy_setopt(curl_, CURLOPT_NOPROGRESS, 0);
-
-            CURLcode ret = curl_easy_perform(curl_);
-            if (ret == CURLE_OK) {
-
-            }
-            ofstream.close();
-
-            curl_easy_cleanup(curl_);
-            return true;
+            curl_easy_cleanup(curl);
+            ho.ofstream->close();
         }
 
-        void get_header(std::string url) {
-            CURL* curl_ = curl_easy_init();
-            curl_easy_setopt(curl_, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl_, CURLOPT_HEADER, 1);    //只需要header头
-            curl_easy_setopt(curl_, CURLOPT_NOBODY, 1);//不得到内容
-            CURLcode ret = curl_easy_perform(curl_);
-            double size = 0;
-            if (ret == CURLE_OK) {
-                curl_easy_getinfo(curl_, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &size);
+
+
+        void post(std::string url, std::map<std::string, std::string>* header, std::map<std::string, std::string>* params,
+                        std::function<void(std::string header, char *data, size_t len)> callback) {
+            CURL *curl = curl_easy_init();
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_POST, 1);
+            curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+            struct curl_slist* list = NULL;
+            if (header != nullptr) {
+                std::map<std::string, std::string>::const_iterator iterator = header->begin();
+                while (iterator != header->end()) {
+                    list = curl_slist_append(list, (iterator->first + ":" + iterator->second).c_str());
+                    iterator ++;
+                }
+                curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
             }
-            curl_easy_cleanup(curl_);
+
+            if (params != nullptr) {
+                struct curl_httppost *formpost=NULL;
+                struct curl_httppost *lastptr=NULL;
+
+                std::map<std::string, std::string>::const_iterator iterator = params->begin();
+                while (iterator != params->end()) {
+                    curl_formadd(&formpost,
+                                 &lastptr,
+                                 CURLFORM_COPYNAME, iterator->first.c_str(),
+                                 CURLFORM_COPYCONTENTS, iterator->second.c_str(),
+                                 CURLFORM_END);
+                    iterator ++;
+                }
+                curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+            }
+            CURLcode ret = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            if (list != NULL) {
+                curl_slist_free_all(list);
+            }
+        }
+
+        void resolve(std::string url) {
+//            curl_slist* host = curl_slist_append(NULL, "example.com:80:127.0.0.1");
+            CURL *curl = curl_easy_init();
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+//            curl_easy_setopt(curl, CURLOPT_RESOLVE, host);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+            curl_easy_setopt(curl, CURLOPT_READFUNCTION, &read_callback);
+            curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, &debug_callback);
+            curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+//            curl_easy_setopt(curl, CURLOPT_SOCKOPTFUNCTION, &sockopt_callback);
+//            curl_easy_setopt(curl, CURLOPT_OPENSOCKETFUNCTION, &opensocket_callback);
+            CURLcode ret = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
         }
 
     private:
+        CURLM *curlm;
     };
-
-
 
     http::http() : impl_(new http_impl) {
 
     }
 
-    void http::get(std::string url, std::function<void(char *, size_t)> callback) {
-        impl_->get(url, callback);
+    http http::instance() {
+        static http h;
+        return h;
     }
 
-    bool http::get(std::string url, std::string file, std::function<void(long, long)> process) {
-        return impl_->get(url, file, process);
+    void http::get(std::string url, std::function<void(std::string header, char *, size_t)> callback) {
+        impl_->get(url, nullptr, callback);
     }
 
-    void http::get_string(std::string url, std::function<void(std::string)> callback) {
-        get(url, [=](char* data, size_t len){
-            std::string ret(data, len);
-            delete(data);
-            callback(ret);
-        });
+    void http::download(std::string url, std::string path, std::function<void(long downloaded, long total)> callback) {
+        impl_->download(url, path, callback);
     }
 
+    void http::get(std::string url, std::map<std::string, std::string> *header,
+                   std::function<void(std::string header, char *data, size_t len)> callback) {
+        impl_->get(url, header, callback);
+    }
+
+    void http::post(std::string url, std::map<std::string, std::string> *header, std::map<std::string, std::string> *params,
+               std::function<void(std::string header, char *data, size_t len)> callback) {
+        impl_->post(url, header, params, callback);
+    }
 }
--- common/src/network/http.h
+++ common/src/network/http.h
@@ -1,31 +1,49 @@
 //
-// Created by liuke on 16/6/3.
+// Created by guazi on 8/4/16.
 //
 
-#ifndef COMMON_HTTP_H
-#define COMMON_HTTP_H
+#ifndef HTTP_HTTP_H
+#define HTTP_HTTP_H
 
-#include <memory>
-#include <string>
 #include <functional>
+#include <string>
+#include <map>
 
 namespace plan9 {
 
+
     class http {
     public:
-        http();
-        //get数据
-        void get(std::string url, std::function<void(char*, size_t)> callback);
-        //下载数据到文件,并有进度通知
-        bool get(std::string url, std::string file, std::function<void(long, long)> process);
+        static http instance();
 
-        void get_string(std::string url, std::function<void(std::string)> callback);
+        /**
+         * 简单的get请求, 只适合于最简单的请求,不能用于下载大文件等操作
+         * @param url 请求的url
+         * @param callback 请求的结果回调
+         */
+        void get(std::string url, std::function<void(std::string header, char *data, size_t len)> callback);
+
+        void get(std::string url, std::map<std::string, std::string>* header, std::function<void(std::string header, char *data, size_t len)> callback);
+
+        /**
+         * 下载文件到本地
+         * @param url 文件的url
+         * @param path 本地文件路径
+         * @param callback 下载进度回调
+         */
+        void download(std::string url, std::string path, std::function<void(long downloaded, long total)> callback);
+
+        void post(std::string url, std::map<std::string, std::string>* header, std::map<std::string, std::string>* params, std::function<void(std::string header, char* data, size_t len)> callback);
 
     private:
+        http();
+
         class http_impl;
+
         std::shared_ptr<http_impl> impl_;
     };
 
 }
 
-#endif //COMMON_HTTP_H
+
+#endif //HTTP_HTTP_H
