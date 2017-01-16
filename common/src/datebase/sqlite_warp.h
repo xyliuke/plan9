c77722c798d52e13fd32f5664f4106bad91f1552
liuke <liuke2@guazi.com>
2017-01-16 12:11:02 +0800

    添加sqlite数据库支持，不能加密

--- common/src/datebase/sqlite_warp.h
+++ common/src/datebase/sqlite_warp.h
@@ -5,10 +5,27 @@
 #ifndef COMMON_SQLITE_WARP_H
 #define COMMON_SQLITE_WARP_H
 
+#include <string>
+#include <functional>
+#include <list>
+#include <json/JSONObject.h>
+
 namespace plan9
 {
+    typedef std::list<JSONObject> result_set;
     class sqlite_warp {
+    public:
+        static sqlite_warp instance();
+        bool open_database(std::string db);
+        void close_database();
+        bool exec(std::string sql);
+        bool exec(std::string sql, std::function<void(std::shared_ptr<result_set>)>);
 
+
+    private:
+        sqlite_warp();
+        class sqlite_warp_impl;
+        std::shared_ptr<sqlite_warp_impl> impl_;
     };
 }
 
